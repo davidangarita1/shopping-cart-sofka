1350d57da0beaab7fe90800b00efdfc8b68b0ae1
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-01-03 15:29:17 -0500

    Parte 4 terminada

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
 
 import '../index.css'
-import { _getPhones } from '../utils/_DATA'
-import { receivePhones } from '../actions/phones'
-import Container from './Container'
+import { _getPhones } from '../utils/_DATA';
+import { receivePhones } from '../actions/phones';
+import Container from './container';
+
 
 class App extends Component {
   componentDidMount(){
@@ -14,18 +16,12 @@ class App extends Component {
       })
   }
   render(){
-    const { phones } = this.props
-  
+ 
     return (
-      <Container phones={phones} />
+      <Container />
     );
   }
 }
 
-function mapStateToProps({phones}){
-  return{
-    phones
-  }
-}
 
-export default connect(mapStateToProps)(App);
+export default connect()(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
-import phones from './phones'  // our phone reducer
+import { combineReducers } from 'redux';
+import phones from './phones';
 
 export default combineReducers({
 	phones,
